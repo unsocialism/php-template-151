103ffd8b05194b6515e91a4593f05b50b2d8f1fe
unsocialism <timoniori@gmail.com>
2017-06-23 17:07:20 +0200

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,72 @@
+-- Adminer 4.2.5 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `like`;
+CREATE TABLE `like` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `isDislike` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `post_id` (`post_id`),
+  CONSTRAINT `like_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `like_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `like` (`id`, `user_id`, `post_id`, `isDislike`) VALUES
+(5,	6,	7,	0),
+(6,	6,	8,	1);
+
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE `post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(255) CHARACTER SET armscii8 NOT NULL,
+  `content` varchar(500) CHARACTER SET armscii8 NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `post_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `post` (`id`, `user_id`, `title`, `content`) VALUES
+(7,	6,	'Test',	'Miau Miau'),
+(8,	6,	'Test2',	'Miauzzz');
+
+DROP TABLE IF EXISTS `right`;
+CREATE TABLE `right` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET armscii8 NOT NULL,
+  `cancratecontent` tinyint(4) NOT NULL,
+  `candeleteanycontent` tinyint(4) NOT NULL,
+  `cancreateuser` tinyint(4) NOT NULL,
+  `candeleteuser` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `right` (`id`, `name`, `cancratecontent`, `candeleteanycontent`, `cancreateuser`, `candeleteuser`) VALUES
+(1,	'admin',	1,	1,	1,	1);
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `right_id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `isActivated` tinyint(4) NOT NULL,
+  `activationCode` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  KEY `right_id` (`right_id`),
+  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`right_id`) REFERENCES `right` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `user` (`id`, `right_id`, `email`, `password`, `isActivated`, `activationCode`) VALUES
+(5,	1,	'ytreyjk@gmail.com',	'$2y$10$l4WE0jTJgWYX/xdIQrj/fuiqxTr..DUYTTE6i9vCUrDqkXAOK3l5u',	1,	'sXmB2kT049FM78u6'),
+(6,	1,	'test245342@gmail.com',	'$2y$10$Z92TfQF78v8CP8X.l1oxuepMfo2ZH5vf5EZ19DThuSPsFRF9keGTy',	1,	'WOrHSwekE5LlCo3z');
+
+-- 2017-06-23 15:04:16
